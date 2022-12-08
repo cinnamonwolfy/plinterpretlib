8469e8c514859c516ab4378c9bae0c436988e9ff
pocketlinux32 <pocketlinux32@gmail.com>
2022-12-07 21:51:17 -0600

    sources :3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # plinterpretlib
-A small and highly customisable shell interpreter inspired by the Unix shell and BASIC
+
+plinterpretlib is a shell interpreter implemented as a library. The base
+library is entirely written in strict and readable C99, and can easily be
+integrated into other programs. The language syntax is heavily inspired by
+the POSIX shell and the BASIC programming language
---
+++ include/pl32-shell.h
@@ -0,0 +1,46 @@
+/*****************************************************************\
+ pl-shell, v0.90
+ (c) 2022 pocketlinux32, Under MPL v2.0
+ Header file
+\*****************************************************************/
+#pragma once
+#include <pl32.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PLSHVAR_NULL 0
+#define PLSHVAR_INT 1
+#define PLSHVAR_STRING 2
+#define PLSHVAR_BOOL 3
+#define PLSHVAR_FLOAT 4
+
+typedef struct plvariable {
+	void* varptr;
+	int type;
+	char* name;
+	bool isMemAlloc;
+} plvariable_t;
+
+typedef struct plfunctionptr {
+	int (*function)(plarray_t*, plmt_t*);
+	char* name;
+} plfunctionptr_t;
+
+typedef struct plshell {
+	plarray_t* commandBuffer;
+	plarray_t* variableBuffer;
+	plarray_t* blockBuffers;
+	bool isInBlock;
+	plmt_t* memTrack;
+} plshell_t;
+
+void setProductStrings(char* productStr, char* srcUrl);
+
+uint8_t plShell(char* cmdline, plshell_t* shellStats);
+void plShellInteractive(char* prompt, bool showHelpAtStart, plshell_t shellStats);
+
+#ifdef __cplusplus
+}
+#endif
